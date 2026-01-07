f2ef8930274bbc4a0ef1678df2d701b5a3360288
shonima <shoni.vinus@gmail.com>
2026-01-07 15:19:28 +0530

    modified 1 files

--- config/db.js
+++ config/db.js
@@ -16,14 +16,12 @@
 // module.exports=connectDB;
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+
 dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI); // options no longer required
     console.log("✅ MongoDB connected successfully");
   } catch (error) {
     console.error("❌ MongoDB connection failed:", error.message);
@@ -32,4 +30,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
